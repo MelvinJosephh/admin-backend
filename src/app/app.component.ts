a4e7249fa11c5efd43aa21ecff60d0f12bfd0634
MelvinJosephh <collinsme.2000@gmail.com>
2024-07-19 23:09:40 +0300

    added categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
 import { FooterComponent } from "./layouts/footer/footer.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { CategoriesComponent } from "./categories/categories.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent, CategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CategoriesComponent } from '../categories/categories.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CategoriesComponent, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
 
 }
